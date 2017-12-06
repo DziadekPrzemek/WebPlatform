7345bebd8a3f89722ec8e66db17675b93cd938c4
Dziadek <dziadek.przemek@gmail.com>
2017-12-06 23:11:06 +0100

    Google map added

--- src/main/java/pl/bpsportal/web/controllers/mapController.java
+++ src/main/java/pl/bpsportal/web/controllers/mapController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ProjectsController {
+public class mapController {
 	@RequestMapping("/map")
-	public String map(){
+	public String mapController(){
 		return "map/map";
 	}
 }
