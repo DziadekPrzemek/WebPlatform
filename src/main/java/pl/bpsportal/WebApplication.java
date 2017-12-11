62e1db418f7a7659e276ff616951ceb4fa2bb70e
Dziadek <dziadek.przemek@gmail.com>
2017-12-11 21:45:12 +0100

    Layout changes, footer added

--- src/main/java/pl/bpsportal/WebApplication.java
+++ src/main/java/pl/bpsportal/WebApplication.java
@@ -2,12 +2,14 @@ package pl.bpsportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class WebApplication {
+public class WebApplication{
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WebApplication.class, args);
 	}
 }
