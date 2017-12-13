a3f70471ed0af36f54768067f56b3bede364731f
Dziadek <dziadek.przemek@gmail.com>
2017-12-13 21:09:17 +0100

    Added polish coding, changing code with thymeleaf, optimalisation of code

--- src/main/java/pl/bpsportal/config/I18NConfig.java
+++ src/main/java/pl/bpsportal/config/I18NConfig.java
@@ -1,16 +1,36 @@
 package pl.bpsportal.config;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import nz.net.ultraq.thymeleaf.decorators.strategies.GroupingStrategy;
+import org.springframework.boot.autoconfigure.web.WebMvcProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+
+import java.util.Locale;
+
 
 @Configuration
 public class I18NConfig {
+
+
 	@Bean
 	public ReloadableResourceBundleMessageSource messageSource(){
 		ReloadableResourceBundleMessageSource resourceBundleMessageSource = new ReloadableResourceBundleMessageSource();
+		resourceBundleMessageSource.setDefaultEncoding("UTF-8");
 		resourceBundleMessageSource.setBasename("classpath:i18n/messages");
 		resourceBundleMessageSource.setCacheSeconds(1800);
+
 		return resourceBundleMessageSource;
 	}
+
+
+
+
 }
