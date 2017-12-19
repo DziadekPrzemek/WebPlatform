100645bb14937c9c31dacb9ab2524f863e6332ff
Dziadek Przemek <dziadekprzemek@gmail.com>
2017-12-19 20:37:50 +0100

    add modal event to calendar

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -1,14 +1,34 @@
-
 $(document).ready(function() {
+
+
     $('#calendar').fullCalendar({
+
         googleCalendarApiKey: 'AIzaSyB7K4nxGLhUqHfTTeZK6tXTUsBxmQwCKTk',
+
         events: {
-            googleCalendarId: '9ljqm1d5fe8ugr86ovv1at7imc@group.calendar.google.com',
+            googleCalendarId: '9ljqm1d5fe8ugr86ovv1at7imc@group.calendar.google.com'
+        },
 
-
-
+        header: {
+            left: 'prev,next today,listWeek',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        eventRender: function (event, element) {
+            element.attr('href', 'javascript:void(0);');
+            element.click(function() {
+                    $("#myModal").modal();
+                    $(".modal-title").html(event.title);
+                $("#eventStartTime").html(moment(event.start).format('LLLL'));
+                $("#eventEndTime").html(moment(event.end).format('LLLL'));
+                $("#eventDescription").html(event.description);
+            });
         }
-    });
 
 
-});
+
+
+
+
+    })});
+
